a648434ef6b79f11d5e39a278591dc254b11199e
muunleit-projects <muunleit@gmail.com>
2023-07-19 07:08:49 +0200

    rearranged a lot of code write errors to log-file

--- cmd/outdated_apps/main.go
+++ cmd/outdated_apps/main.go
@@ -1,34 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 	"os/exec"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 	"github.com/muunleit-projects/show_updates/pkg/checkupdates"
 )
 
 func main() {
+	const logfile string = "/tmp/outdated_apps.log"
+	var s string
+
 	a := app.New()
 	w := a.NewWindow("outdated Apps")
+	w.Resize(fyne.Size{Width: 200})
 	upgrades, err := checkupdates.Upgradable()
 
 	switch {
 	case err != nil:
-		w.SetContent(
-			widget.NewLabel(
-				fmt.Sprint("Error: ", err.Error(), "\n", upgrades)))
+		wErr := writeError(err)
+		if wErr != nil {
+			s = "Error: " + wErr.Error()
+			break
+		}
+		s = "Error: look at the logfile: " + logfile
 	case upgrades != "":
-		w.SetContent(
-			widget.NewButton(upgrades, func() {
-				exec.Command("open", "/System/Applications/Utilities/Terminal.app").Run()
-				a.Quit()
-			}))
+		s = upgrades
 	default:
-		w.SetContent(
-			widget.NewLabel("no updates"))
+		s = "no updates"
 	}
 
+	w.SetContent(widget.NewButton(s, func() {
+		exec.Command("open", "/System/Applications/Utilities/Terminal.app").Run()
+		a.Quit()
+	}))
+
 	w.ShowAndRun()
+}
+
+func writeError(err error) error {
+	f, ferr := os.OpenFile("/tmp/outdated_apps.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0o644)
+	if ferr != nil {
+		return ferr
+	}
+	defer func() {
+		f.Close()
+	}()
+
+	l := log.Default()
+	l.SetOutput(f)
+	l.Println(err.Error())
+	return nil
 }

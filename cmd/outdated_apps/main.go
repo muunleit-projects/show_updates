61d9e2d044711725ac3084320a18c23f44c9e45e
muunleit-projects <muunleit@gmail.com>
2023-08-18 10:27:21 +0200

    open Terminal from app

--- cmd/outdated_apps/main.go
+++ cmd/outdated_apps/main.go
@@ -16,68 +16,50 @@ import (
 const logfile string = "/tmp/outdated_apps.log"
 
 func main() {
-	var c fyne.CanvasObject
-
 	a := app.New()
 	w := a.NewWindow("outdated Apps")
+
 	w.Resize(fyne.Size{Width: 200})
 
+	openTerminal := func() {
+		exec.Command("open", "/System/Applications/Utilities/Terminal.app").Run()
+		a.Quit()
+	}
+
 	upgrades, err := checkupdates.Upgradable()
+
 	switch {
 	case err != nil:
 		err := logError(err)
 		if err != nil {
-			c = widget.NewLabel("Error: " + err.Error())
+			w.SetContent(widget.NewLabel("Error: " + err.Error()))
 		}
-		c = widget.NewLabel("Error: see " + logfile)
+
+		w.SetContent(widget.NewLabel("Error: see " + logfile))
 	case len(upgrades) == 0 || upgrades == "":
-		c = widget.NewLabel("no updates")
+		w.SetContent(widget.NewLabel("no updates"))
 	default:
-		c = container.NewVBox(
+		w.SetContent(container.NewVBox(
 			widget.NewLabel(upgrades),
 			widget.NewButton("open Terminal", openTerminal),
-		)
+		))
 	}
 
-	w.SetContent(c)
 	w.ShowAndRun()
 }
 
-// func makeUI() fyne.CanvasObject {
-// 	upgrades, err := checkupdates.Upgradable()
-// 	if err != nil {
-// 		err := logError(err)
-// 		if err != nil {
-// 			return widget.NewLabel("Error: " + err.Error())
-// 		}
-// 		return widget.NewLabel("Error: " + logfile)
-// 	}
-// 	if upgrades == "" {
-// 		return widget.NewLabel("no updates")
-// 	}
-// 	return container.NewVBox(
-// 		widget.NewLabel(upgrades),
-// 		widget.NewButton("open Terminal", openTerminal),
-// 	)
-// }
-
 func logError(err error) error {
-	f, fError := os.OpenFile(
-		logfile,
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
-		0o644)
+	f, fError := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 	if fError != nil {
 		return errors.Join(fError, err)
 	}
+
 	defer func() {
 		f.Close()
 	}()
 
 	l := log.New(f, "", log.LstdFlags)
 	l.Println(err.Error())
-	return nil
-}
 
-func openTerminal() {
-	exec.Command("open", "/System/Applications/Utilities/Terminal.app").Run()
+	return nil
 }
